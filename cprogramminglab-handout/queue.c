7541432047c764b7dd7cdbd4724507e82207f040
root <root@DESKTOP-LBO3N78.localdomain>
2021-07-24 14:45:42 +0800

    lab0 done

---
+++ cprogramminglab-handout/queue.c
@@ -0,0 +1,191 @@
+/* 
+ * Code for basic C skills diagnostic.
+ * Developed for courses 15-213/18-213/15-513 by R. E. Bryant, 2017
+ * Modified to store strings, 2018
+ */
+
+/*
+ * This program implements a queue supporting both FIFO and LIFO
+ * operations.
+ *
+ * It uses a singly-linked list to represent the set of queue elements
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "harness.h"
+#include "queue.h"
+
+/*
+  Create empty queue.
+  Return NULL if could not allocate space.
+*/
+queue_t *q_new()
+{
+  queue_t *q = malloc(sizeof(queue_t));
+  /* What if malloc returned NULL? */
+  if (q == NULL)
+    return NULL;
+  q->head = NULL;
+  q->tail = NULL;
+  q->sz = 0;
+  return q;
+}
+
+/* Free all storage used by queue */
+void q_free(queue_t *q)
+{
+  /* How about freeing the list elements and the strings? */
+  /* Free queue structure */
+  if (q == NULL)
+    return;
+  while (q->head != NULL)
+  {
+    list_ele_t *old_head = q->head;
+    q->head = q->head->next;
+    if (old_head->value != NULL)
+      free(old_head->value);
+    free(old_head);
+  }
+  free(q);
+}
+
+/*
+  Attempt to insert element at head of queue.
+  Return true if successful.
+  Return false if q is NULL or could not allocate space.
+  Argument s points to the string to be stored.
+  The function must explicitly allocate space and copy the string into it.
+ */
+bool q_insert_head(queue_t *q, char *s)
+{
+  list_ele_t *newh;
+  /* What should you do if the q is NULL? */
+  if (q == NULL)
+    return false;
+  newh = malloc(sizeof(list_ele_t));
+  /* Don't forget to allocate space for the string and copy it */
+  /* What if either call to malloc returns NULL? */
+  if (newh == NULL)
+    return false;
+  newh->value = malloc(strlen(s) + 1);
+  if (newh->value == NULL)
+  {
+    free(newh);
+    return false;
+  }
+  strcpy(newh->value, s);
+
+  newh->next = q->head;
+  q->head = newh;
+  if (q->sz == 0)
+  {
+    q->tail = q->head;
+  }
+  q->sz += 1;
+  return true;
+}
+
+/*
+  Attempt to insert element at tail of queue.
+  Return true if successful.
+  Return false if q is NULL or could not allocate space.
+  Argument s points to the string to be stored.
+  The function must explicitly allocate space and copy the string into it.
+ */
+bool q_insert_tail(queue_t *q, char *s)
+{
+  /* You need to write the complete code for this function */
+  /* Remember: It should operate in O(1) time */
+  if (q == NULL)
+    return false;
+  list_ele_t *newtail = malloc(sizeof(list_ele_t));
+  if (newtail == NULL)
+    return false;
+  newtail->value = malloc(strlen(s) + 1);
+  if (newtail->value == NULL)
+  {
+    free(newtail);
+    return false;
+  }
+  strcpy(newtail->value, s);
+  newtail->next = NULL;
+  if (q->sz == 0)
+  {
+    q->head = newtail;
+    q->tail = q->head;
+  }
+  else
+  {
+    q->tail->next = newtail;
+    q->tail = q->tail->next;
+  }
+  q->sz += 1;
+  return true;
+}
+
+/*
+  Attempt to remove element from head of queue.
+  Return true if successful.
+  Return false if queue is NULL or empty.
+  If sp is non-NULL and an element is removed, copy the removed string to *sp
+  (up to a maximum of bufsize-1 characters, plus a null terminator.)
+  The space used by the list element and the string should be freed.
+*/
+bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
+{
+  /* You need to fix up this code. */
+  if (q == NULL || q->head == NULL)
+    return false;
+  list_ele_t *tmp = q->head;
+  q->head = q->head->next;
+  if (sp != NULL)
+  {
+    strncpy(sp, tmp->value, bufsize - 1);
+    sp[bufsize - 1] = '\0';
+  }
+  free(tmp->value);
+  free(tmp);
+  q->sz -= 1;
+  return true;
+}
+
+/*
+  Return number of elements in queue.
+  Return 0 if q is NULL or empty
+ */
+int q_size(queue_t *q)
+{
+  /* You need to write the code for this function */
+  /* Remember: It should operate in O(1) time */
+  if (q == NULL)
+    return 0;
+  return q->sz;
+}
+
+/*
+  Reverse elements in queue
+  No effect if q is NULL or empty
+  This function should not allocate or free any list elements
+  (e.g., by calling q_insert_head, q_insert_tail, or q_remove_head).
+  It should rearrange the existing ones.
+ */
+void q_reverse(queue_t *q)
+{
+  /* You need to write the code for this function */
+  if (q == NULL || q->head == NULL)
+    return;
+  list_ele_t *newhead = NULL;
+  list_ele_t *newtail = q->head;
+  while (q->head != q->tail)
+  {
+    list_ele_t *tmp = q->head;
+    q->head = q->head->next;
+    tmp->next = newhead;
+    newhead = tmp;
+  }
+  q->head->next = newhead;
+  q->tail = newtail;
+}
